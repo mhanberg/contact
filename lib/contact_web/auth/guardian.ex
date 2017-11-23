8edc1b060ee75e5fb1a90563a7a5863b7fe9b455
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2017-11-23 12:02:57 -0500

    Removed redundant function clauses in Guardian modules.
    These functions would never get called, I'm not sure why the
    documentation has these four functions.

--- lib/contact_web/auth/guardian.ex
+++ lib/contact_web/auth/guardian.ex
@@ -5,15 +5,7 @@ defmodule ContactWeb.Guardian do
     {:ok, to_string(resource.id)}
   end
 
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
-  end
-
   def resource_from_claims(_claims) do
     {:ok, :ok}
-  end
-
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
   end
 end
