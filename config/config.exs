ebcb65fcb2001ef3f7faa9f0e3907efd4f2787a7
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2018-01-29 21:50:24 -0500

    format a couple files I missed when upgrading to 1.6.0

--- config/config.exs
+++ config/config.exs
@@ -6,16 +6,14 @@
 use Mix.Config
 
 # General application configuration
-config :contact,
-  ecto_repos: [Contact.Repo]
+config :contact, ecto_repos: [Contact.Repo]
 
 # Configures the endpoint
 config :contact, ContactWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "ze7PM4JDIZ92E0qOfx8JJSJrMIzQryTWUlk1X/9sfaEr5zodAy6zBje9pArA8fjF",
   render_errors: [view: ContactWeb.ErrorView, accepts: ~w(html json json-api)],
-  pubsub: [name: Contact.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Contact.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -23,8 +21,7 @@ config :logger, :console,
   metadata: [:request_id]
 
 # Configures JaSerializer
-config :phoenix, :format_encoders,
-  "json-api": Poison
+config :phoenix, :format_encoders, "json-api": Poison
 
 config :mime, :types, %{
   "application/vnd.api+json" => ["json-api"]
@@ -37,4 +34,4 @@ config :contact, ContactWeb.Guardian,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
