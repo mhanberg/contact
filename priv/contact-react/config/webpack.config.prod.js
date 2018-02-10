01abefcfb9044f0004b9843f005f9ee0bd5fb85b
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2018-02-10 00:29:14 -0500

    Add elm prod webpack config.

--- priv/contact-react/config/webpack.config.prod.js
+++ priv/contact-react/config/webpack.config.prod.js
@@ -88,7 +88,7 @@ module.exports = {
     // https://github.com/facebookincubator/create-react-app/issues/290
     // `web` extension prefixes have been added for better support
     // for React Native Web.
-    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
+    extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx', '.elm'],
     alias: {
       
       // Support React Native Web
@@ -212,6 +212,13 @@ module.exports = {
             ),
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
+          {
+            rules: [{
+              test: /\.elm$/,
+              exclude: [/elm-stuff/, /node_modules/],
+              loader: require.resolve('elm-webpack-loader')
+            }]
+          },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
           // This loader doesn't use a "test" so it will catch all modules
@@ -222,7 +229,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.elm$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
