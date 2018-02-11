6cab653d9b607d751289652ba4bbcbe948087b15
Mitchell Hanberg <the.mitch.hanberg@gmail.com>
2018-02-10 22:30:25 -0500

    Non functional message box and send button

--- priv/contact-react/src/elm/View.elm
+++ priv/contact-react/src/elm/View.elm
@@ -12,9 +12,17 @@ view : Model -> Html Msg
 view model =
     div []
         [ div [] [ text (model.roomId |> Maybe.withDefault "999") ]
-        , div [ class "well chat-box", id "chatBox" ]
-            [ div [ class "message-log" ]
-                (List.map renderMessage model.messages)
+        , div [ class "row" ]
+            [ div [ class "col-xs-12" ]
+                [ div [ class "well chat-box", id "chatBox" ]
+                    [ div [ class "message-log" ]
+                        (List.map renderMessage model.messages)
+                    ]
+                ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "col-xs-8 col-sm-10" ] [ input [ placeholder "Type your message...", class "form-control" ] [] ]
+            , div [ class "col-xs-4 col-sm-2" ] [ button [ class "btn btn-danger" ] [ text "Send!" ] ]
             ]
         ]
 
